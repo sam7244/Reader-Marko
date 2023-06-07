fc0f528f4b7d94f31cbcb4c9027e97e2f2d4fb2f
Vishnu <vappu5429@gmail.com>
2023-06-07 23:30:09 +0530

    Feature--ReactToPdf

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import * as xlsx from "xlsx";
+import React, { useRef } from "react";
+
 import { useState, useEffect } from "react";
 import DropZone from "./DropZone";
 import Spreadsheet from "react-spreadsheet";
+
 import { OutTable, ExcelRenderer } from "react-excel-renderer";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 const FileUpload = () => {
+  const ref = useRef();
   const [cols, setCols] = useState([]);
   const [rows, setRows] = useState([]);
 
@@ -22,10 +26,23 @@ const FileUpload = () => {
     });
   };
 
+  const ConverToPdf = () => {
+    const capture = document.querySelector("#table");
+    html2canvas(document.body).then((canvas) => {
+      const imgData = canvas.toDataURL("img/png");
+      const doc = new jsPDF("p", "mm", "a4");
+      const componentWidth = doc.internal.pageSize.getWidth();
+      const componentHeight = doc.internal.pageSize.getHeight();
+      doc.addImage(imgData, "PNG", 0, 0, componentWidth, componentHeight);
+      doc.save("Document.pdf");
+    });
+  };
+
   return (
     <div className=" p-10  overflow-hidden">
       <div className="flex flex-col p-4 gap-2  items-center justify-center">
         <p className="font-bold text-xl animate-bounce">Upload File Below</p>
+
         <input
           className=" py-5 text-md  font-semibold px-4 bg-gray-200 rounded-lg"
           type="file"
@@ -35,7 +52,8 @@ const FileUpload = () => {
           onChange={readUploadFile}
         />
       </div>
-      <div className="max-h-[60vh] overflow-y-scroll">
+      <button onClick={ConverToPdf}>Download</button>
+      <div id="table" className=" overflow-y-scroll">
         <OutTable
           data={rows}
           columns={cols}

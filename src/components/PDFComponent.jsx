62ca00bcc0dfd5861c9289f4c66059f48cb8e599
shamith <shamithkumar7244@gmail.com>
2023-06-08 01:38:46 +0530

    Not--sure_commit

--- src/components/PDFComponent.jsx
+++ src/components/PDFComponent.jsx
@@ -1,18 +1,14 @@
-
-import ReactPDF from '@react-pdf/renderer';
-import { PDFViewer, Document, Page, Text } from '@react-pdf/renderer';
-
+import ReactPDF from "@react-pdf/renderer";
+import { PDFViewer, Document, Page, Text } from "@react-pdf/renderer";
 
 const PDFComponent = ({ data }) => (
-    <Document>
-      <Page>
-        {data.map((row, index) => (
-          <Text key={index}>
-            {Object.values(row).join('\t')}
-          </Text>
-        ))}
-      </Page>
-    </Document>
-  );
-  
+  <Document className="w-[100rem] h-full">
+    <Page>
+      {data.map((row, index) => (
+        <Text key={index}>{Object.values(row).join("\t")}</Text>
+      ))}
+    </Page>
+  </Document>
+);
+
 export default PDFComponent;

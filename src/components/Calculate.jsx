fd2da02f069ec1d79785dfdd2073fdac17cd1788
shamith <shamithkumar7244@gmail.com>
2023-06-19 22:18:41 +0530

    feature--added-frontend-pdf-mapped_Data

--- src/components/Calculate.jsx
+++ src/components/Calculate.jsx
@@ -1,162 +1,169 @@
 import React from "react";
 
-const weights = {
-  H: 1,
-  M: 0.6,
-  L: 0.4,
-};
+const Calculate = ({ mappedData, setmappedData }) => {
+  // Calculate the updated Table 2 data
 
-const table2Data = [
-  [
-    "Course Outcomes",
-    "PO",
-    "PO",
-    "PO",
-    "PO",
-    "PO",
-    "PO",
-    "PO",
-    "PO",
-    "PO",
-    "PO",
-    "PO",
-    "PSO",
-    "PSO",
-  ],
-  [null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2],
-  [
-    "CO1",
-    "H",
-    "H",
-    null,
-    null,
-    "H",
-    null,
-    null,
-    null,
-    "M",
-    null,
-    null,
-    null,
-    "H",
-    "H",
-  ],
-  [
-    "CO2",
-    "H",
-    "H",
-    null,
-    null,
-    "H",
-    null,
-    null,
-    null,
-    "H",
-    null,
-    null,
-    null,
-    "H",
-    "H",
-  ],
-  [
-    "CO3",
-    "H",
-    "H",
-    null,
-    null,
-    "H",
-    null,
-    null,
-    null,
-    "M",
-    null,
-    null,
-    null,
-    "H",
-    "H",
-  ],
-  [
-    "CO4",
-    "H",
-    "H",
-    null,
-    null,
-    "H",
-    null,
-    null,
-    null,
-    "H",
-    null,
-    null,
-    null,
-    "H",
-    "H",
-  ],
-  [
-    "CO5",
-    "H",
-    "H",
-    null,
-    null,
-    "H",
-    null,
-    null,
-    null,
-    "M",
-    null,
-    null,
-    null,
-    "H",
-    "H",
-  ],
-];
+  const weights = {
+    H: 1,
+    M: 0.6,
+    L: 0.4,
+  };
 
-const table4Data = [
-  ["Course Outcomes", "CO Attainment", "Low", "Medium", "High"],
-  ["CO1", 4.5, 1.8, 2.7, 4.5],
-  ["CO2", 4.5, 1.8, 2.7, 4.5],
-  ["CO3", 5, 2, 3, 5],
-  ["CO4", 5, 2, 3, 5],
-  ["CO5", 5, 2, 3, 5],
-];
+  const table2Data = [
+    [
+      "Course Outcomes",
+      "PO",
+      "PO",
+      "PO",
+      "PO",
+      "PO",
+      "PO",
+      "PO",
+      "PO",
+      "PO",
+      "PO",
+      "PO",
+      "PO",
+      "PSO",
+      "PSO",
+    ],
+    [null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2],
+    [
+      "CO1",
+      "H",
+      "H",
+      null,
+      null,
+      "H",
+      null,
+      null,
+      null,
+      "M",
+      null,
+      null,
+      null,
+      "H",
+      "H",
+    ],
+    [
+      "CO2",
+      "H",
+      "H",
+      null,
+      null,
+      "H",
+      null,
+      null,
+      null,
+      "H",
+      null,
+      null,
+      null,
+      "H",
+      "H",
+    ],
+    [
+      "CO3",
+      "H",
+      "H",
+      null,
+      null,
+      "H",
+      null,
+      null,
+      null,
+      "M",
+      null,
+      null,
+      null,
+      "H",
+      "H",
+    ],
+    [
+      "CO4",
+      "H",
+      "H",
+      null,
+      null,
+      "H",
+      null,
+      null,
+      null,
+      "H",
+      null,
+      null,
+      null,
+      "H",
+      "H",
+    ],
+    [
+      "CO5",
+      "H",
+      "H",
+      null,
+      null,
+      "H",
+      null,
+      null,
+      null,
+      "M",
+      null,
+      null,
+      null,
+      "H",
+      "H",
+    ],
+  ];
 
-const calculateTable2Data = (table2Data, table4Data) => {
-  const updatedTable2Data = [];
+  const table4Data = [
+    ["Course Outcomes", "CO Attainment", "Low", "Medium", "High"],
+    ["CO1", 4.5, 1.8, 2.7, 4.5],
+    ["CO2", 4.5, 1.8, 2.7, 4.5],
+    ["CO3", 5, 2, 3, 5],
+    ["CO4", 5, 2, 3, 5],
+    ["CO5", 5, 2, 3, 5],
+  ];
 
-  for (let i = 0; i < table2Data.length; i++) {
-    const row = [...table2Data[i]];
+  const calculateTable2Data = (table2Data, table4Data) => {
+    const updatedTable2Data = [];
 
-    if (i > 1) {
-      for (let j = 1; j < row.length; j++) {
-        if (row[j] === "H" || row[j] === "M" || row[j] === "L") {
-          const co = row[0];
-          const coAttainment = table4Data.find((row) => row[0] === co)[1];
-          const lowValue = table4Data.find((row) => row[0] === co)[2];
-          const mediumValue = table4Data.find((row) => row[0] === co)[3];
-          const highValue = table4Data.find((row) => row[0] === co)[4];
+    for (let i = 0; i < table2Data.length; i++) {
+      const row = [...table2Data[i]];
 
-          if (row[j] === "H") {
-            row[j] = highValue;
-          } else if (row[j] === "M") {
-            row[j] = mediumValue;
-          } else if (row[j] === "L") {
-            row[j] = lowValue;
+      if (i > 1) {
+        for (let j = 1; j < row.length; j++) {
+          if (row[j] === "H" || row[j] === "M" || row[j] === "L") {
+            const co = row[0];
+            const coAttainment = table4Data.find((row) => row[0] === co)[1];
+            const lowValue = table4Data.find((row) => row[0] === co)[2];
+            const mediumValue = table4Data.find((row) => row[0] === co)[3];
+            const highValue = table4Data.find((row) => row[0] === co)[4];
+
+            if (row[j] === "H") {
+              row[j] = highValue;
+            } else if (row[j] === "M") {
+              row[j] = mediumValue;
+            } else if (row[j] === "L") {
+              row[j] = lowValue;
+            }
           }
         }
       }
+
+      updatedTable2Data.push(row);
     }
+    setmappedData(updatedTable2Data);
+    return updatedTable2Data;
+  };
 
-    updatedTable2Data.push(row);
-  }
+  //   const updatedTable2Data = calculateTable2Data(table2Data, table4Data);
+  //console.log(updatedTable2Data);
 
-  return updatedTable2Data;
-};
-// Calculate the updated Table 2 data
-const updatedTable2Data = calculateTable2Data(table2Data, table4Data);
-
-const Calculate = () => {
-  console.log(updatedTable2Data);
-  return <div>Calculate</div>;
+  return (
+    <button onClick={calculateTable2Data(table2Data, table4Data)}>
+      push Me
+    </button>
+  );
 };
 
 export default Calculate;
--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -14,8 +14,9 @@ import "jspdf-autotable";
 import { client } from "../../lib/client";
 import data from "../../utils/getData";
 import OutputTable from "./OutputTable";
-
+import OutputTableThird from "./OutputTableThird";
 import Calculate from "./Calculate";
+import calculateTable2Data from "../../utils/calculateTable2Data";
 
 const FileUpload = ({ id }) => {
   const [Threshold, setThreshold] = useState(0);
@@ -24,6 +25,7 @@ const FileUpload = ({ id }) => {
   const [rows, setRows] = useState([]);
   const [isUploaded, setIsUploaded] = useState(false);
   const [attainment, setAttainment] = useState([]);
+  const [mappedData, setmappedData] = useState([]);
 
   const readUploadFile = async (e) => {
     let fileObj = e.target.files[0];
@@ -177,6 +179,52 @@ const FileUpload = ({ id }) => {
         }
       },
     });
+    doc.addPage();
+
+    addHeading("Mapped Data");
+    doc.autoTable({
+      head: [mappedData[0], mappedData[1]], // Use the first row as table headers
+      body: mappedData.slice(2), // Exclude the first row from table body
+      startY: 20, // Set the initial y-coordinate for the table
+      theme: "grid",
+
+      styles: {
+        fontSize: 7,
+        cellPadding: 5,
+        textColor: [1, 1, 0],
+      },
+      columnStyles: {
+        // Add more column styles as needed
+      },
+      didDrawPage: function (mappedData) {
+        const { table, pageNumber } = mappedData;
+        const totalPages = doc.internal.getNumberOfPages();
+
+        if (pageNumber === totalPages) {
+          // Check if the table height exceeds the available space on the page
+          if (table.height > doc.internal.pageSize.getHeight() - 20) {
+            doc.addPage(); // Add a new page
+            doc.autoTable({
+              head: [mappedData[0], mappedData[1]], // Repeat the table headers on the new page
+              body: mappedData.slice(2), // Use the remaining body data
+              startY: 20, // Set the initial y-coordinate for the table on the new page
+
+              styles: {
+                fontSize: 2,
+                cellPadding: 10,
+                textColor: [0, 0, 0],
+              },
+              columnStyles: {
+                0: { cellWidth: "auto" },
+                1: { cellWidth: "auto" },
+                2: { cellWidth: "auto" },
+                // Add more column styles as needed
+              },
+            });
+          }
+        }
+      },
+    });
 
     doc.addPage();
 
@@ -256,141 +304,8 @@ const FileUpload = ({ id }) => {
     setCols(cols);
 
     setRows(updatedData);
-  };
-
-  const threshhold = () => {
-    const THRESH_HOLD = 60;
-    const STUDENT_COUNT = data.length;
-
-    var u1Sum = 0;
-    var u2Sum = 0;
-    var u3Sum = 0;
-    var u4Sum = 0;
-
-    let thirty_per = parseInt((30 * STUDENT_COUNT) / 100);
-    let forty_per = parseInt((40 * STUDENT_COUNT) / 100);
-    let fifty_per = parseInt((50 * STUDENT_COUNT) / 100);
-    let sixty_per = parseInt((60 * STUDENT_COUNT) / 100);
-    let seventy_per = parseInt((70 * STUDENT_COUNT) / 100);
-
-    let u1 = ((THRESH_HOLD * 8) / 100 - 0.1).toFixed(1);
-    let u2 = ((THRESH_HOLD * 7) / 100 - 0.1).toFixed(1);
-    let u3 = ((THRESH_HOLD * 8) / 100 - 0.1).toFixed(1);
-    let u4 = ((THRESH_HOLD * 7) / 100 - 0.1).toFixed(1);
-
-    var u1Sum = 0;
-
-    for (var i = 1; i < data.length; i++) {
-      if (data[i][0] > u1) {
-        u1Sum++;
-      }
-      if (data[i][1] > u2) {
-        u2Sum++;
-      }
-      if (data[i][2] > u3) {
-        u3Sum++;
-      }
-      if (data[i][3] > u3) {
-        u4Sum++;
-      }
-    }
-
-    let U1Level;
-    let U2Level;
-    let U3Level;
-    let U4Level;
-
-    if (u1Sum > thirty_per && u1Sum < forty_per) {
-      U1Level = "L1";
-    } else if (u1Sum > forty_per && u1Sum < fifty_per) {
-      U1Level = "L2";
-    } else if (u1Sum > fifty_per && u1Sum < sixty_per) {
-      U1Level = "L3";
-    } else if (u1Sum > sixty_per && u1Sum < seventy_per) {
-      U1Level = "L4";
-    } else if (u1Sum > seventy_per && u1Sum < STUDENT_COUNT) {
-      U1Level = "L5";
-    } else {
-      U1Level = "Invalid";
-    }
-
-    if (u2Sum > thirty_per && u2Sum < forty_per) {
-      U2Level = "L1";
-    } else if (u2Sum > forty_per && u2Sum < fifty_per) {
-      U2Level = "L2";
-    } else if (u2Sum > fifty_per && u2Sum < sixty_per) {
-      U2Level = "L3";
-    } else if (u2Sum > sixty_per && u2Sum < seventy_per) {
-      U2Level = "L4";
-    } else if (u2Sum > seventy_per && u2Sum < STUDENT_COUNT) {
-      U2Level = "L5";
-    } else {
-      U2Level = "Invalid";
-    }
-
-    if (u3Sum > thirty_per && u3Sum < forty_per) {
-      U3Level = "L1";
-    } else if (u3Sum > forty_per && u3Sum < fifty_per) {
-      U3Level = "L2";
-    } else if (u1Sum > fifty_per && u1Sum < sixty_per) {
-      U1Level = "L3";
-    } else if (u3Sum > sixty_per && u3Sum < seventy_per) {
-      U3Level = "L4";
-    } else if (u3Sum > seventy_per && u3Sum < STUDENT_COUNT) {
-      U3Level = "L5";
-    } else {
-      U3Level = "Invalid";
-    }
-
-    if (u4Sum > thirty_per && u4Sum < forty_per) {
-      U4Level = "L1";
-    } else if (u4Sum > forty_per && u4Sum < fifty_per) {
-      U4Level = "L2";
-    } else if (u1Sum > fifty_per && u1Sum < sixty_per) {
-      U1Level = "L3";
-    } else if (u4Sum > sixty_per && u4Sum < seventy_per) {
-      U4Level = "L4";
-    } else if (u4Sum > seventy_per && u4Sum < STUDENT_COUNT) {
-      U4Level = "L5";
-    } else {
-      U4Level = "Invalid";
-    }
-
-    const LevelArrayCIE = [U1Level, U2Level, U3Level, U4Level, "L5"];
-    const LevelArraySIE = [U1Level, U2Level, U3Level, U4Level, "L5"];
-    const SumArrayCIE = [u1Sum, u2Sum, u3Sum, u4Sum, 60];
-    const SumArraySIE = [u1Sum, u2Sum, u3Sum, u4Sum, 60];
-    const NameArray = ["C502.1", "C502.2", "C502.3", "C502.4", "C502.5"];
-
-    const LevelMap = new Map();
-
-    LevelMap.set("L1", 1);
-    LevelMap.set("L2", 2);
-    LevelMap.set("L3", 3);
-    LevelMap.set("L4", 4);
-    LevelMap.set("L5", 5);
-
-    let AttainmentData = [];
-
-    for (let i = 0; i < 5; i++) {
-      var AvgAttainent = (
-        (parseInt(LevelMap.get(LevelArrayCIE[i])) +
-          parseInt(LevelMap.get(LevelArraySIE[i]))) /
-        2
-      ).toFixed(1);
-
-      var PercetAttainment = (100 * AvgAttainent) / 5;
-
-      AttainmentData.push([
-        NameArray[i],
-        SumArrayCIE[i],
-        parseInt(LevelMap.get(LevelArrayCIE[i])),
-        SumArraySIE[i],
-        parseInt(LevelMap.get(LevelArraySIE[i])),
-        AvgAttainent,
-        PercetAttainment,
-      ]);
-    }
+    const updatedTable2Data = calculateTable2Data();
+    setmappedData(updatedTable2Data);
   };
 
   return (
@@ -443,17 +358,16 @@ const FileUpload = ({ id }) => {
                   setAttainment={setAttainment}
                 />
               </div>
+
               <div className="grid w-full justify-center items-center">
-                <OutputTableSec
+                <OutputTableThird
                   rows={rows}
                   isUploaded={isUploaded}
                   setIsUploaded={setIsUploaded}
                   handleChange={handleChange}
-                  attainment={attainment}
-                  setAttainment={setAttainment}
+                  mappedData={mappedData}
                 />
               </div>
-              <Calculate />
             </div>
             <div
               style={{ borderRadius: "10px" }}

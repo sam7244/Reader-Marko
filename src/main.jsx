19dd312687558b9b390001989796773fd1824b0d
Vishnu <vappu5429@gmail.com>
2023-07-06 11:54:43 +0530

    Added Context

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 import { Toaster } from "react-hot-toast";
+import { StateContextProvider } from "../utils/Context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Toaster />
-      <App />
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
